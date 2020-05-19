828dfc2223de5b6269becd1cab466c2f904a7769
Alberto Ferreira <alberto.ferreira@feedzai.com>
2020-05-19 01:46:22 +0100

    clox.error: Fix missing set on *had-error*

--- clox-error.lisp
+++ clox-error.lisp
@@ -5,10 +5,11 @@
 
 (defparameter *had-error* nil "Lox's error state - yup we're not using Lisp's condition system :D")
 
-(defun lox-error (line message)
+(defun clox-error (line message)
   (declare (integer line))
   (report line "" message))
 
 (defun report (line where message)
   (declare (integer line))
-  (format *error-output* "[line ~A] Error~A: ~A" line where message))
+  (format *error-output* "[line ~A] Error~A: ~A~%" line where message)
+  (setf *had-error* T))
