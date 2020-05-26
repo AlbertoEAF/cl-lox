1765bda9ebe3f22b29d49d0466e03c0e1170c597
Alberto Ferreira <alberto.ferreira@feedzai.com>
2020-05-26 01:47:01 +0100

    Add runtime type-checks to interpreter expression arguments

--- interpreter.lisp
+++ interpreter.lisp
@@ -4,14 +4,8 @@
                     (#:token  #:lox.token)))
 (in-package :lox.interpreter)
 
-(defmethod evaluate ((expr syntax:literal))
-  (slot-value expr 'syntax:value))
-
-(defmethod evaluate ((expr syntax:grouping))
-  (evaluate (slot-value expr 'syntax:expression)))
-
-(defmethod evaluate ((expr syntax:expr))
-  expr)
+;; In the book the methods `evaluate` are called `visit-{literal,unary,...}`.
+;; As we don't need the visitor pattern there isn't the class interpreter either.
 
 (defun truthy-p (expr)
   (case expr
@@ -21,15 +15,6 @@
 (defun not-truthy-p (expr)
   (if (truthy-p expr) :f
       :t))
-
-
-(defmethod evaluate ((expr syntax:unary))
-  (let ((right (evaluate (slot-value expr 'syntax:right)))
-        (operator-token-type (token:get-token-type (slot-value expr 'syntax:operator))))
-    (cond ((token:token-type= 'MINUS operator-token-type)
-           (- right))
-          ((token:token-type= 'BANG operator-token-type)
-           (not-truthy-p right)))))
 
 (defun type? (type-specifier &rest vars)
   "Ensure all vars are of type type-specifier."
@@ -43,12 +28,46 @@
         ((lox-null a) nil)
         (t (equal a b))))
 
+(define-condition lox-runtime-error (error)
+  ((token :initarg :token
+          :accessor token)
+   (message :initarg :message
+            :accessor message))
+  (:documentation "Lox Runtime Error."))
+
+(defun* check-number-operand ((operator token:token) operand)
+  (when (not (typep operand 'number))
+    (error 'lox-runtime-error :token operator :message "Operand must be a number.")))
+
+(defun* check-number-operands ((operator token:token) left right)
+  (when (not (type? 'number left right))
+    (error 'lox-runtime-error :token operator :message "Operands must be numbers.")))
+
+
+(defmethod evaluate ((expr syntax:expr))
+  expr)
+
+(defmethod evaluate ((expr syntax:literal))
+  (slot-value expr 'syntax:value))
+
+(defmethod evaluate ((expr syntax:grouping))
+  (evaluate (slot-value expr 'syntax:expression)))
+
+(defmethod evaluate ((expr syntax:unary))
+  (let ((right (evaluate (slot-value expr 'syntax:right)))
+        (operator-token-type (token:get-token-type (slot-value expr 'syntax:operator))))
+    (cond ((token:token-type= 'MINUS operator-token-type)
+           (- right))
+          ((token:token-type= 'BANG operator-token-type)
+           (not-truthy-p right)))))
+
 (defmethod evaluate ((expr syntax:binary))
   (let* ((left (evaluate (slot-value expr 'syntax:left)))
          (right (evaluate (slot-value expr 'syntax:right)))
+         (operator (slot-value expr 'syntax:operator))
          (operator-token-type (intern
                                (string-upcase
-                                (token:get-token-type (slot-value expr 'syntax:operator)))))
+                                (token:get-token-type operator))))
          (simple-op (case operator-token-type
                       (SLASH '/)
                       (STAR '*)
@@ -57,15 +76,17 @@
                       (GREATER_EQUAL '>=)
                       (LESS '<)
                       (LESS_EQUAL '<=))))
-    (if simple-op (funcall simple-op left right)
+    (if simple-op (progn (check-number-operands operator left right)
+                         (funcall simple-op left right))
         (case operator-token-type
           (PLUS
            (cond ((type? 'string left right)
                   (format nil "~A~A" left right))
+                 ((type? 'number left right)
+                  (+ left right))
                  (t
-                  (+ left right))))
+                  (error 'lox-runtime-error :token operator
+                                            :message "Operators must be two numbers or two strings."))))
           (BANG_EQUAL (not (is-equal left right)))
           (EQUAL_EQUAL (is-equal left right))
           (t nil)))))
-
-
